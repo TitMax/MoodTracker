3b95fa59109bc62875a0488cc8a98309ca2e568f
Maxime Boussaingault <maxime.boussaingault@gmail.com>
2017-11-15 14:39:29 +0100

    Added addNote and history button and the AddNoteDialog function

--- app/src/main/java/com/boussaingault/maxime/moodtracker/activities/MainActivity.java
+++ app/src/main/java/com/boussaingault/maxime/moodtracker/activities/MainActivity.java
@@ -1,10 +1,18 @@
 package com.boussaingault.maxime.moodtracker.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.boussaingault.maxime.moodtracker.R;
 import com.boussaingault.maxime.moodtracker.fragments.SmileyFragment;
@@ -13,14 +21,36 @@ import com.boussaingault.maxime.moodtracker.models.VerticalViewPager;
 public class MainActivity extends FragmentActivity {
 
     private VerticalViewPager mVerticalViewPager;
+    private ImageButton mImageButtonAddNote;
+    private ImageButton mImageButtonHistory;
+    private EditText mEditTextNote;
+    private String note = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mVerticalViewPager = findViewById(R.id.activity_main_view_pager);
+        mImageButtonAddNote = (ImageButton) findViewById(R.id.activity_main_note_add_btn);
+        mImageButtonHistory = (ImageButton) findViewById(R.id.activity_main_history_btn);
+
         mVerticalViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
         mVerticalViewPager.setCurrentItem(3);
+
+        mImageButtonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddNoteDialog();
+            }
+        });
+
+        mImageButtonHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private class MyPagerAdapter extends FragmentPagerAdapter {
@@ -41,6 +71,37 @@ public class MainActivity extends FragmentActivity {
         public int getCount() {
             return 5;
         }
+    }
+
+    private void AddNoteDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this, R.style.AlertDialogCustom);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.layout_add_note_dialog, null, false);
+        dialogBuilder.setView(dialogView);
+
+        mEditTextNote = (EditText) dialogView.findViewById(R.id.layout_add_note_edit_text);
+        mEditTextNote.setText(note);
+
+        dialogBuilder.setPositiveButton("Valider", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (mEditTextNote.getText().toString().equals(""))
+                    Toast.makeText(MainActivity.this, "Aucune note trouvée, annulation", Toast.LENGTH_SHORT).show();
+                else {
+                    note = mEditTextNote.getText().toString();
+                    Toast.makeText(MainActivity.this, "Note ajoutée", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        dialogBuilder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Annulation", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
     }
 
     @Override
