64ecc316ea30a5f4ba6777a5d69fb32ab9d6bd2b
Maxime Boussaingault <maxime.boussaingault@gmail.com>
2017-11-21 14:33:56 +0100

    Added comments

--- app/src/main/java/com/boussaingault/maxime/moodtracker/models/CustomToastMessage.java
+++ app/src/main/java/com/boussaingault/maxime/moodtracker/models/CustomToastMessage.java
@@ -15,6 +15,7 @@ import com.boussaingault.maxime.moodtracker.R;
  * Created by Tit Max on 21/11/2017.
  */
 
+// Method to create a custom toast message
 public class CustomToastMessage {
 
     public static void showMessage(Context context, String message) {
@@ -27,6 +28,7 @@ public class CustomToastMessage {
 
         Toast toast = new Toast(context);
         toast.setGravity(Gravity.BOTTOM, 0, 50);
+        // Extend the toast duration if the message exceeds 50 characters
         if (message.length() > 50)
             toast.setDuration(Toast.LENGTH_LONG);
         else
